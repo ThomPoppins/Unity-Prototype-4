a39df6924277b88d4999b692c989d2da1c2ad5d7
Thom Veldpaus <thompoppins@gmail.com>
2023-10-12 09:29:35 +0100

    Move random spawn position generation to it's own methed

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -1,19 +1,28 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
     public GameObject enemyPrefab; // Enemy prefab to spawn
+    private float spawnRange = 9.0f; // Spawn range
     // Start is called before the first frame update
     void Start()
     {
-        Instantiate(enemyPrefab, new Vector3(0, 0, 6), enemyPrefab.transform.rotation); // Spawn an enemy prefab at the spawn position
+        Instantiate(enemyPrefab, GenerateRandomSpawnPosition(), enemyPrefab.transform.rotation); // Spawn an enemy prefab at the spawn position
     }
 
     // Update is called once per frame
     void Update()
     {
 
+    }
+
+    private Vector3 GenerateRandomSpawnPosition() // Generate a random spawn position
+    {
+        float spawnPosX = Random.Range(-spawnRange, spawnRange); // Randomize the spawn position on the x axis
+        float spawnPosZ = Random.Range(-spawnRange, spawnRange); // Randomize the spawn position on the z axis
+
+        Vector3 randomPos = new Vector3(spawnPosX, 0, spawnPosZ); // Create a spawn position Vector3
+
+        return randomPos; // Return the spawn position
     }
 }
