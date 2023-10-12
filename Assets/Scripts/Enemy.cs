4bf8aae0aa20e54f1e03e6eb57c15bdc5fcc1798
Thom Veldpaus <thompoppins@gmail.com>
2023-10-12 09:01:37 +0100

    Make enemy move towards the player

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    public float speed = 2.0f; // Movement speed
+    private Rigidbody enemyRb; // Enemy's Rigidbody component
+    private GameObject player; // Player GameObject
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        enemyRb = GetComponent<Rigidbody>(); // Get the enemy's Rigidbody component
+        player = GameObject.Find("Player"); // Find the player GameObject
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 lookDirection = (player.transform.position - transform.position).normalized; // Get the direction from the enemy to the player
+
+        enemyRb.AddForce(lookDirection * speed); // Move the enemy towards the player, normalized force
     }
 }
